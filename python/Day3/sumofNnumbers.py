658376e0d4af67c463f28932b827d9f2997a2e78
pavankumar21-tech <pavankumarm9866@gmail.com>
2026-07-13 16:51:59 +0530

    Day 3 python and day1 DSA completed

---
+++ python/Day3/sumofNnumbers.py
@@ -0,0 +1,5 @@
+n=int(input("enter the number"))
+sum=0
+for i in range(n):
+    sum+=i
+print(sum)
---
+++ python/Day3/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+a=int(input("enter a number"))
+for i in range(1,10):
+    print(a,"*",i,"=",a*i)
